dd17ec15517f3400e349c1091d065a5ebbf3b6c6
josh <josh-moss@live.nmit.ac.nz>
2020-08-31 09:44:34 +1200

    added how to set up jQuery

---
+++ jQuery.js
@@ -0,0 +1,35 @@
+/*              
+
+***** Ways to set up jQuery *****
+
+
+
+Local Installation - Downloaded to the local machine and then inclueded into the HTML code. 
+
+CDN based version - Allows for us to Link the libary Through Content Delivery Network (CDN).
+
+For this activity i will be using the CDN method. Which steps i have listed below.
+
+
+
+For google CND the code below is used:
+ 
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+
+
+
+Microsoft CDN code below:
+
+ 
+<head>
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+</head>
+
+
+According to the jQuery Tutorial we are working though using the CDN linked method is faster
+compared to local version. This is only faster by a few milliseconds. but seems to be the better 
+choice for me.
+
+*/
